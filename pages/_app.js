341f4be35dc519e3b9c1e2fc7594c5a8a2dfb281
Mono <siritaporn.mar@gmail.com>
2023-10-08 13:42:31 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import '../styles/userStyle/userCSS.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
