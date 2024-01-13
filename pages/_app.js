8acb3f20bd495f0340d1febf13c29fcd98e3c113
Mono <siritaporn.mar@gmail.com>
2024-01-14 00:55:16 +0700

    add layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import '../styles/userStyle/userCSS.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { popupReducer, PopupContext, initPopupState } from '../providers/popupcontext'
+import { memberReducer, MemberContext, initMemberState } from '../providers/membercontext'
+import { layoutReducer, LayoutContext, initLayoutState } from '../providers/layoutcontext'
+import { useReducer } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [popupState, popupDispatch] = useReducer(popupReducer, initPopupState)
+  const [memberState, memberDispatch] = useReducer(memberReducer, initMemberState)
+  const [layoutState, layoutDispatch] = useReducer(layoutReducer, initLayoutState)
+
+  return (
+    <PopupContext.Provider value={{ popupState, popupDispatch }}>
+      <MemberContext.Provider value={{ memberState, memberDispatch }}>
+        <LayoutContext.Provider value={{ layoutState, layoutDispatch }}>
+          <Component {...pageProps} />
+        </LayoutContext.Provider>
+      </MemberContext.Provider>
+    </PopupContext.Provider>
+  )
 }
--- pages/user/login.js
+++ pages/user/login.js
@@ -1,63 +1,39 @@
-import React, { useState } from "react";
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box'
-import { Stack,Button,Alert } from "@mui/material";
-import NestedModal from "../../components/utils/modal";
+import React, { useContext, useState } from "react";
+import UserComponent from "../../components/user/usercomponent";
+import { UserLogin } from "../../services/userservices/userservice";
+import { MemberContext } from "../../providers/membercontext";
+import { LayoutContext } from "../../providers/layoutcontext";
+import Waitingsignal from "../../components/layouts/waitingsignal";
 
 const login = () => {
-    const [openModal, setOpenModal] = useState(false)
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState(""); 
+    const {memberState,memberDispatch} = useContext(MemberContext)
+    const {layoutState,layoutDispatch} = useContext(LayoutContext)
 
-    const showData = async () =>{
-        // console.log(`username : ${username} password : ${password}`);
-        // alert(`username : ${username} password : ${password}`)
-        setTimeout(() => setOpenModal(true), 1000);
-        console.log(openModal);
-    }
-    const handleClose = () =>{
-        setOpenModal(false);
+    const onLogon = async () =>{
+        layoutDispatch({type:'SET_DISPLAY',payload:{display:true}})
+        const data = await UserLogin({userId:'',username:memberState.userLogin.email,password:memberState.userLogin.password})
+        if(data){
+            localStorage.setItem('token',data.auth)
+        }
+        alert(`${JSON.stringify(data)}`)
+        layoutDispatch({type:'SET_DISPLAY',payload:{display:false}})
     }
 
     return (
-        <div className="layout_main">
+        <>
+        <Waitingsignal></Waitingsignal>
+        <div className="layout_member">
             <div className="box_login">
-                
-                <h1 className="user_header">Login</h1>
-                <Stack spacing={2} className="stack_01">
-                <TextField className={""}
-                    id="outlined-basic"
-                    label="username" variant="outlined"
-                    defaultValue={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    InputLabelProps={{ style: { color: "black" } }}
-                    InputProps={{ style: { color: "gray", border: '0px solid gray',width:'500px' } }}
-                />
-                 <TextField className={""}
-                    id="outlined-required"
-                    label="password" variant="outlined" type="password"
-                    defaultValue={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    InputLabelProps={{ style: { color: "black" } }}
-                    InputProps={{ style: { color: "gray",width:'500px' } }}
-                />
-                <div>
-
-                <Button variant="contained" size="large" 
-                style={{color:'black'}} onClick={() => showData()}>
-          Send
-        </Button>
+                    <UserComponent 
+                    topic='Login' 
+                    inputEmailDisplay='true'
+                    inputPasswordDisplay='true'
+                    onClickLogon= {()=>onLogon()}
+                    ></UserComponent>
                 </div>
-                </Stack>
-                
-                <NestedModal openModal={openModal} 
-                onCloseModal={handleClose}
-                message={"Test"}/>
-               
-            </div>
         </div>
+        </>
     )
 }
-
 
 export default login
