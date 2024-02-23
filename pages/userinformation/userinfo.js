513a218672c32d20e98332d1c923be5b8a906151
Mono <siritaporn.mar@gmail.com>
2024-02-24 01:49:57 +0700

    add new side bar

--- pages/userinformation/userinfo.js
+++ pages/userinformation/userinfo.js
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import Mainheader from "../../components/layouts/main/mainheader";
 import Mainnavbar from "../../components/layouts/main/mainnavbar";
 import MainTopbar from "../../components/layouts/main/maintopbar";
+import MainSidebar from "../../components/layouts/main/mainsidebar";
 import { MemberContext } from "../../providers/membercontext";
 import { GetUserDirectory, UpdateUserDirectory } from "../../services/userservices/userservice";
 import { LayoutContext } from "../../providers/layoutcontext";
@@ -69,7 +70,7 @@ export default function userinfo() {
 
     const onLoadData = async () => {
         openWaitingSign();
-        try{
+        try {
 
             const data = await GetUserDirectory();
             if (data && data.result.status == true) {
@@ -78,46 +79,48 @@ export default function userinfo() {
                 memberDispatch({ type: "SET_PHONE", payload: { phone: data.userDirectory.phone ?? '' } })
                 memberDispatch({ type: "SET_EMAIL_INFO", payload: { email: data.userDirectory.email ?? '' } })
             }
-        }catch(error){
+        } catch (error) {
             console.log(error)
-        }finally{
+        } finally {
             closeWaitingSign()
         }
-            
+
     }
 
     return (
         <>
             <MainTopbar></MainTopbar>
+            <MainSidebar></MainSidebar>
             {/* <Mainheader /> */}
-            <Mainnavbar />
-            <div className="main-content-info">
-                <div className='topic-content-info'>
-                    <h1 onClick={ontestjson}>Information</h1>
-                </div>
-                <div className="grid-content-info">
-                    <label className="label-info" htmlFor='fname'>Firstname :</label>
-                    <input className="input-info" id="fname" type="text" placeholder="Firstname" disabled onChange={(e) => { onChangeFirstname(e) }} value={memberState.userInfo.firstname}></input>
-
-                    <label className="label-info" htmlFor="lname">Lastname :</label>
-                    <input className="input-info" id="lname" type="text" placeholder="Lastname" disabled onChange={(e) => { onChangeLastname(e) }} value={memberState.userInfo.lastname}></input>
-
-                    <label className="label-info" htmlFor="email">Email :</label>
-                    <input className="input-info" id="email" type="text" placeholder="Email" disabled value={memberState.userInfo.email}></input>
-
-                    <label className="label-info" htmlFor='phone'>Phone :</label>
-                    <input className="input-info" id="phone" type="text" placeholder="Phone" disabled onChange={(e) => { onChangePhone(e) }} value={memberState.userInfo.phone}></input>
-                </div>
-                <div className="grid-btn-info">
-                    <div className="div-btn-left">
-                        <button className="btn-border-red" onClick={onEnableBtn}>Edit</button>
+            {/* <Mainnavbar /> */}
+            <div className="main-content">
+                <div className="main-content-info">
+                    <div className='topic-content-info'>
+                        <h1 onClick={ontestjson}>Information</h1>
                     </div>
-                    <div className="div-btn-right">
-                        <button id="btnUpdate" type="button" className="btn-border-blue" onClick={() => { onUpdateInfo() }}>Update</button>
+                    <div className="grid-content-info">
+                        <label className="label-info" htmlFor='fname'>Firstname :</label>
+                        <input className="input-info" id="fname" type="text" placeholder="Firstname" disabled onChange={(e) => { onChangeFirstname(e) }} value={memberState.userInfo.firstname}></input>
+
+                        <label className="label-info" htmlFor="lname">Lastname :</label>
+                        <input className="input-info" id="lname" type="text" placeholder="Lastname" disabled onChange={(e) => { onChangeLastname(e) }} value={memberState.userInfo.lastname}></input>
+
+                        <label className="label-info" htmlFor="email">Email :</label>
+                        <input className="input-info" id="email" type="text" placeholder="Email" disabled value={memberState.userInfo.email}></input>
+
+                        <label className="label-info" htmlFor='phone'>Phone :</label>
+                        <input className="input-info" id="phone" type="text" placeholder="Phone" disabled onChange={(e) => { onChangePhone(e) }} value={memberState.userInfo.phone}></input>
+                    </div>
+                    <div className="grid-btn-info">
+                        <div className="div-btn-left">
+                            <button className="btn-border-red" onClick={onEnableBtn}>Edit</button>
+                        </div>
+                        <div className="div-btn-right">
+                            <button id="btnUpdate" type="button" className="btn-border-blue" onClick={() => { onUpdateInfo() }}>Update</button>
+                        </div>
                     </div>
                 </div>
             </div>
-
 
         </>
     )
